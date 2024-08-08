f6547afa8aa41f048d963f9cab15cdfeef377e10
R3TICULAR <98478018+R3TICULAR@users.noreply.github.com>
2024-08-08 19:08:01 -0400

    calculator js file added to experiment with functions

---
+++ Calculator-app.js
@@ -0,0 +1,14 @@
+const Addnums = (a , b) => {
+    let sum;
+    sum = a+b;
+    return sum;
+}
+
+console.log("Hello world!");
+
+
+let juices = 20;
+
+let fries = 2;
+
+console.log(Addnums(juices , fries));
